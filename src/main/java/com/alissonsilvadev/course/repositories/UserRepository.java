2dc4f8f3351184e1947d8521d411872f47635cd1
Alisson Silva <camargoalisson92@hotmail.com>
2024-10-21 06:23:01 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/alissonsilvadev/course/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.alissonsilvadev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.alissonsilvadev.course.entities.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+}
