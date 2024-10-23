4760b811e991fcd44c2e6ea030b8789507b152aa
Alisson Silva <camargoalisson92@hotmail.com>
2024-10-23 04:52:49 -0300

    Ajuste para gravar dados no banco H2

--- src/main/java/com/alissonsilvadev/course/CourseApplication.java
+++ src/main/java/com/alissonsilvadev/course/CourseApplication.java
@@ -2,8 +2,10 @@ package com.alissonsilvadev.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.alissonsilvadev.config")
 public class CourseApplication {
 
 	public static void main(String[] args) {
