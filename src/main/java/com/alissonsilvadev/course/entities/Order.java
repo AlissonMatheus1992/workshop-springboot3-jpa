8fd42641f29ac5b14ee74484fc787e65e11387cd
Alisson Silva <camargoalisson92@hotmail.com>
2024-11-17 07:08:05 -0300

    Subtotal & Total methods

--- src/main/java/com/alissonsilvadev/course/entities/Order.java
+++ src/main/java/com/alissonsilvadev/course/entities/Order.java
@@ -93,8 +93,6 @@ public class Order implements Serializable {
 		return items;
 	}
 	
-	
-
 	public Payment getPayment() {
 		return payment;
 	}
@@ -102,7 +100,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
